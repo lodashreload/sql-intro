4afe1d1bd36a1c6eb7d11fd5d199b169a8516eb8
lodashreload <icarusjapan@gmail.com>
2022-01-06 20:23:24 -0500

    added employees

--- queries.sql
+++ queries.sql
@@ -6,3 +6,25 @@ CREATE TABLE "Employee" (
   "IsPartTime" BOOLEAN
 );
 
+SELECT "FullName" FROM "Employee"
+
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Danny OneChins', 10000, 'Enforcer', 2, FALSE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Johnny Boss', 100000, 'Boss', 2, FALSE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Tony OneShot', 50000, 'Right Hand', 2, FALSE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Tony OtherTony', 5000, 'Bookie', 2, TRUE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Tony TwoFingers', 10000, 'Bookie2', 2, TRUE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Chuck Rookie', 1000, 'Runner', 2, TRUE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Markus The Brick', 10000, 'Problem Solver', 2, FALSE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Nickie Vixen', 10000, 'Front', 2, false);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Mama', 100000, 'QA', 2, TRUE);
+INSERT INTO "Employee" ("FullName",  "Salary", "JobPosition", "PhoneExtention", "IsPartTime")
+VALUES ('Danny TwoChins', 10000, 'Enforcer', 2, TRUE);
